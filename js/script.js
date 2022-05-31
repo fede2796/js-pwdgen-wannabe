9d6bcd4720012b4b39bcf0e4780517200c911611
fede2796 <fede27396@gmail.com>
2022-05-31 15:35:05 +0200

    modifiche js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Password: chiediamo all'utente il suo nome, cognome e colore preferito e restituiamo tutti insieme
 
 // Chiedo il nome 
-const username = prompt ('Dimmi il tuo nome');
+const username = prompt('Dimmi il tuo nome');
 
 // Chiedo il cognome
-const usersurname = prompt ('Dimmi il tuo cognome');
+const usersurname = prompt('Dimmi il tuo cognome');
 
 //Chiedo il colore preferito
-const usercolor = prompt ('Dimmi il tuo colore preferito :');
+const usercolor = prompt('Dimmi il tuo colore preferito :');
 
 //Password Generator
 const usermessage = username + usersurname + usercolor + 21 ;
